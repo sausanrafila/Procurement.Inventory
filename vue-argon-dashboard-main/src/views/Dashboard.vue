70c1239c7390446d1c8c207db1c4a9afc2ee9c3e
MeisyAnjarfika <meisyanjarfika@student.uns.ac.id>
2023-06-13 13:13:32 +0700

    add dashboard

--- vue-argon-dashboard-main/src/views/Dashboard.vue
+++ vue-argon-dashboard-main/src/views/Dashboard.vue
@@ -55,13 +55,14 @@
           </div>
         </div>
         <div class="row mt-4">
-          <div class="col-lg-7 mb-lg-0 mb-4">
+          <div class="col-lg-12 mb-lg-0 mb-4">
             <div class="card">
               <div class="p-3 pb-0 card-header">
                 <div class="d-flex justify-content-between">
                   <h6 class="mb-2">Latest Request</h6>
                 </div>
               </div>
+              
               <div class="table-responsive">
                 <table class="table align-items-center">
                   <tbody>
@@ -69,15 +70,13 @@
                     <div class="text-center">
                     <p class="mb-0 text-xs font-weight-bold">Member Name</p>
                     </div>
-                    
 
                     <tr v-for="(sale, index) in sales" :key="index">
                       
                       <td class="w-30">
                   
-                        <div class="px-2 py-1 d-flex align-items-center">
-                          
-                          <div class="ms-4">
+                        <div class="text-center">
+                          <div class="ms-4 ">
                             <h6 class="mb-0 text-sm">{{ sale.country }}</h6>
                           </div>
                         </div>
@@ -130,9 +129,7 @@
 </template>
 <script>
 import Card from "@/examples/Cards/Card.vue";
-import GradientLineChart from "@/examples/Charts/GradientLineChart.vue";
-import Carousel from "./components/Carousel.vue";
-import CategoriesCard from "./components/CategoriesCard.vue";
+
 
 import US from "@/assets/img/icons/flags/US.png";
 import DE from "@/assets/img/icons/flags/DE.png";
@@ -200,9 +197,6 @@ export default {
   },
   components: {
     Card,
-    GradientLineChart,
-    Carousel,
-    CategoriesCard,
   },
 };
 </script>
